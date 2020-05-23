14ef9346cb5be9b34539bf7e39fda5de8640a84f
Nicolas Hurtado <nhd15c@gmail.com>
2020-05-22 20:41:14 -0400

    Added missing Bootstrap stylesheet and reorganized global sass file
    * Changed base font-size calculation.
    * Moved everything out of global scope to comply with Next.js documentation.

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,6 +13,12 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Lato&family=Source+Sans+Pro:wght@700&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+            crossOrigin="anonymous"
+            rel="stylesheet"
+          />
         </Head>
 
         <body>
