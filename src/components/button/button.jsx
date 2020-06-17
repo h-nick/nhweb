4f190e45a2e8d2021586e7f1a18264dbb9a43ce6
Nicolas Hurtado <nhd15c@gmail.com>
2020-06-17 03:17:39 -0400

    Button component now passed down classNames and includes improved styles

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,33 +1,43 @@
 import React from 'react';
 import clsx from 'clsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 import classes from './component.module.scss';
 
 const Button = ({
-  href, title, newTab, blackVariation, width,
+  href, title, newTab, blackVariation, className,
 }) => {
   let component;
+  const text = (
+    <>
+      { title }
+      <FontAwesomeIcon icon={faChevronRight} />
+    </>
+  );
 
   if (newTab) {
     component = (
       <a
-        className={clsx(classes.Button, !blackVariation && 'white')}
+        className={clsx(classes.Button, !blackVariation && 'white', className)}
         href={href}
         target="_blank"
         rel="noopener noreferrer"
-        style={{ width: `${width}%` }}
       >
-        { title }
+        <div className={classes.Text}>
+          { text }
+        </div>
       </a>
     );
   } else {
     component = (
       <a
-        className={clsx(classes.Button, !blackVariation && 'white')}
+        className={clsx(classes.Button, !blackVariation && 'white', className)}
         href={href}
-        style={{ width: `${width}%` }}
       >
-        { title }
+        <div className={classes.Text}>
+          { text }
+        </div>
       </a>
     );
   }
@@ -39,14 +49,14 @@ Button.propTypes = {
   href: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   newTab: PropTypes.bool,
-  width: PropTypes.number,
+  className: PropTypes.string,
   blackVariation: PropTypes.bool,
 };
 
 Button.defaultProps = {
   blackVariation: false,
   newTab: false,
-  width: 100,
+  className: '',
 };
 
 export default Button;
