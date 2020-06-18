3dc377b582869227408a96c7f76520f55e4be666
Nicolas Hurtado <nhd15c@gmail.com>
2020-06-18 18:23:23 -0400

    Created preloader component

---
+++ src/components/loader-animation/loader-animation.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './component.module.scss';
+
+const LoaderAnimation = () => (
+  <div className={classes.LoaderAnimation} sr-only="Loading" />
+);
+
+export default LoaderAnimation;
