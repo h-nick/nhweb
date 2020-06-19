67b0c0701f73a99ea90fb976684359c7d3394cbf
Nicolas Hurtado <nhd15c@gmail.com>
2020-06-18 23:41:41 -0400

    Added missing components to Projects page

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import apiInstance from '../../utils/api-instance';
+import Button from '../../components/button/button';
+import ContactBlock from '../../components/contact-block/contact-block';
+import Footer from '../../components/footer/footer';
+import LoaderAnimation from '../../components/loader-animation/loader-animation';
+import LoaderError from '../../components/loader-error/loader-error';
 import Navbar from '../../components/navbar/navbar';
 import ProjectBlock from '../../components/project-block/project-block';
 import classes from './index.module.scss';
@@ -8,96 +13,123 @@ import classes from './index.module.scss';
 const Projects = () => {
   const [projects, setProjects] = useState(undefined);
   const [loading, setLoading] = useState(true);
+  const [loadingError, setLoadingError] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
+
     (async () => {
       await apiInstance.get('/projects?_sort=id:DESC')
         .then((response) => {
           setProjects(response.data);
           setLoading(false);
+          setLoadingError(false);
         })
         .catch(() => {
           setLoading(false);
+          setLoadingError(true);
         });
     })();
   }, []);
 
   return (
     <>
-      <div className={classes.Top}>
-        <Navbar />
+      {
+        loadingError && <LoaderError />
+      }
 
-        <div className={classes.Content}>
-          <h1 className="highlight-variation big-size">Some projects</h1>
-          <h1 className="white big-size">I&apos;ve worked on</h1>
-
-          <div>
-            <button type="button">PLACEHOLDER</button>
+      {
+        loading && (
+          <div className="loader-bg">
+            <LoaderAnimation />
           </div>
-        </div>
-      </div>
+        )
+      }
 
-      <Container className="py-5 my-5">
-        <Row>
-          <Col className="px-5">
-            <h4 className="text-center">
-              Although nowadays I mostly work as a Full-Stack Web Developer, I have
-              worked in a range of projects. This is a collection of both work and
-              personal projects. Something something placeholdertext
-            </h4>
-          </Col>
-        </Row>
-      </Container>
+      {
+        !loading && (
+          <>
+            <div className={classes.Top}>
+              <Navbar />
 
-      <Container className="mb-5">
-        <Row>
-          {
-            !loading && (!projects || !projects.length) && (
-              <Col>
-                <h3 className="text-center">
-                  Apparently there are no projects published :(
-                </h3>
-              </Col>
-            )
-          }
+              <div className={classes.Content}>
+                <h1 className="highlight-variation big-size">Some projects</h1>
+                <h1 className="white big-size">I&apos;ve worked on</h1>
 
-          {
-            /* eslint-disable no-nested-ternary */
-            /* ESlint rule disabled for simplicity */
+                <div>
+                  <button type="button">PLACEHOLDER</button>
+                </div>
+              </div>
+            </div>
 
-            /*
-              Note for future self: Is the classname too messy?
-              I think it's better than wasting time splitting the array
-              and generating new rows.
-            */
-          }
+            <Container className="py-5 my-5">
+              <Row>
+                <Col className="px-5">
+                  <h4 className="text-center">
+                    Although nowadays I mostly work as a Full-Stack Web Developer, I have
+                    worked in a range of projects. This is a collection of both work and
+                    personal projects. Something something placeholdertext
+                  </h4>
+                </Col>
+              </Row>
+            </Container>
 
-          {
-            !loading
-            && projects
-            && projects.length > 0
-            && projects.map((project, index) => (
-              <Col xs={12} md={6} lg={4} key={project.id}>
-                <ProjectBlock
-                  title={project.title}
-                  slug={project.slug}
-                  mainPhoto={project.main_photo.url}
-                  className={
-                    `${index !== 0 && index === 1
-                      ? 'mt-4 mt-md-0'
-                      : index === 2
-                        ? 'mt-4 mt-lg-0'
-                        : index > 2
-                          ? 'mt-4'
-                          : ''
-                    }`
-                  }
-                />
-              </Col>
-            ))
-          }
-        </Row>
-      </Container>
+            <Container className="mb-5">
+              <Row>
+                {
+                  !loading && (!projects || !projects.length) && (
+                    <Col>
+                      <h3 className="text-center">
+                        Apparently there are no projects published :(
+                      </h3>
+                    </Col>
+                  )
+                }
+
+                {
+                  /* eslint-disable no-nested-ternary */
+                  /* ESlint rule disabled for simplicity */
+
+                  /*
+                    Note for future self: Is the classname too messy?
+                    I think it's better than wasting time splitting the array
+                    and generating new rows.
+                  */
+                }
+
+                {
+                  !loading
+                  && projects
+                  && projects.length > 0
+                  && projects.map((project, index) => (
+                    <Col xs={12} md={6} lg={4} key={project.id}>
+                      <ProjectBlock
+                        title={project.title}
+                        slug={project.slug}
+                        mainPhoto={project.main_photo.url}
+                        className={
+                          `${index !== 0 && index === 1
+                            ? 'mt-4 mt-md-0'
+                            : index === 2
+                              ? 'mt-4 mt-lg-0'
+                              : index > 2
+                                ? 'mt-4'
+                                : ''
+                          }`
+                        }
+                      />
+                    </Col>
+                  ))
+                }
+              </Row>
+            </Container>
+
+            <ContactBlock />
+
+            <Footer />
+          </>
+        )
+      }
     </>
   );
 };
