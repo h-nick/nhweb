067fcb334a7722e059562f38db03277ed0d7549b
Nicolas Hurtado <nhd15c@gmail.com>
2020-07-27 22:10:36 -0400

    Equalized the heights of contact blocks

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import clsx from 'clsx';
 import { Container, Row, Col } from 'react-bootstrap';
-import axios from 'axios';
 import AnimatedChevron from '../../components/animated-chevron/animated-chevron';
 import Footer from '../../components/footer/footer';
 import Navbar from '../../components/navbar/navbar';
@@ -36,64 +33,61 @@ const Projects = () => (
         <Col md={4}>
           <div className={clsx(classes.ContactBlock, 'rising-effect p-3')}>
             <div className={classes.Text}>
-              <h3>placeholder</h3>
+              <h4>Hire me</h4>
 
               <p className="m-0">
-                This is just a placeholder text to fill up the space
-                in the contact block. Placeholding text is very important
-                from time to time.
+                Are you looking for a professional web developer or have a job offer?
+                Submit your message and I&apos;ll get back to you.
               </p>
 
-              <span className="title highlight">Contact me</span>
+              <span className="title highlight">SEND INFO</span>
             </div>
           </div>
         </Col>
-        <Col md={4}>
+        <Col md={4} className="mt-3 mt-md-0">
           <div className={clsx(classes.ContactBlock, 'rising-effect p-3')}>
             <div className={classes.Text}>
-              <h3>placeholder</h3>
+              <h4>Support request</h4>
 
               <p className="m-0">
-                This is just a placeholder text to fill up the space
-                in the contact block. Placeholding text is very important
-                from time to time.
+                Are you a client having problems with a service or product?
+                Send a message and I will respond as soon as possible.
               </p>
 
-              <span className="title highlight">Contact me</span>
+              <span className="title highlight">REQUEST SUPPORT</span>
             </div>
           </div>
         </Col>
-        <Col md={4}>
+        <Col md={4} className="mt-3 mt-md-0">
           <div className={clsx(classes.ContactBlock, 'rising-effect p-3')}>
             <div className={classes.Text}>
-              <h3>placeholder</h3>
+              <h4>General inquiry</h4>
 
               <p className="m-0">
-                This is just a placeholder text to fill up the space
-                in the contact block. Placeholding text is very important
-                from time to time.
+                Send me any question you have about the services I offer, the development process
+                or a friendly hello anytime you want.
               </p>
 
-              <span className="title highlight">Contact me</span>
+              <span className="title highlight">SUBMIT MESSAGE</span>
             </div>
           </div>
         </Col>
       </Row>
 
       <Row className="mt-3">
-        <Col md={6}>
+        <Col sm={6}>
           <a
             className={clsx(classes.ContactBlock, 'rising-effect p-3 d-block')}
             href="tel://+580000000000"
           >
             <div className={classes.Text}>
-              <h3>You can also contact me through Whatsapp</h3>
+              <h4>Contact me through Whatsapp</h4>
 
               <span className="title highlight">At +58 000 000 0000</span>
             </div>
           </a>
         </Col>
-        <Col md={6}>
+        <Col sm={6} className="mt-3 mt-sm-0">
           <a
             className={clsx(classes.ContactBlock, 'rising-effect p-3 d-block')}
             href="https://t.me/telegramusername"
@@ -101,7 +95,7 @@ const Projects = () => (
             rel="noopener noreferrer"
           >
             <div className={classes.Text}>
-              <h3>Our send me a message through Telegram</h3>
+              <h4>Or through Telegram</h4>
 
               <span className="title highlight">At @telegramusername</span>
             </div>
