fc604b8bca0ea72c80b11c4220a23435e6cb26c6
Nicolas Hurtado <nhd15c@gmail.com>
2020-07-29 22:06:35 -0400

    Integrated ImageLoader to ProjectBlock

--- src/components/project-block/project-block.jsx
+++ src/components/project-block/project-block.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
+import ImageLoader from '../image-loader/image-loader';
 import classes from './component.module.scss';
 
-const ProjectBlock = ({
-  title, slug, mainPhoto, className,
-}) => (
-  <div className={clsx(classes.Project, className)}>
-    <div className={classes.Data}>
-      <h4 className="white text-center m-0 px-2">{title}</h4>
-      <button type="button" className="mt-2 mx-auto d-block text-center">
-        PLACEHOLDER
-      </button>
-    </div>
+const ProjectBlock = (props) => {
+  const {
+    title, slug, mainPhoto, className,
+  } = props;
 
-    <div className={classes.Cover} />
+  return (
+    <div className={clsx(classes.Project, className)}>
+      <div className={classes.Data}>
+        <h4 className="white text-center m-0 px-2">{title}</h4>
+        <button type="button" className="mt-2 mx-auto d-block text-center">
+          PLACEHOLDER
+        </button>
+      </div>
 
-    <div className={classes.Image}>
-      <img src={`https://api.nicolashurtado.dev${mainPhoto}`} alt={slug} />
+      <div className={classes.Cover} />
+
+      <div className={classes.Image}>
+        <ImageLoader
+          src={`https://api.nicolashurtado.dev${mainPhoto}`}
+          alt={slug}
+          className={classes.Image}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 ProjectBlock.propTypes = {
   title: PropTypes.string.isRequired,
