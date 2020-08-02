38eab48d243df7d24dd633036812c896928cbf6d
Nicolas Hurtado <nhd15c@gmail.com>
2020-08-01 21:55:07 -0400

    Installed NProgress

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import NProgress from 'nprogress';
 import PropTypes from 'prop-types';
+import Router from 'next/router';
 import '../styles/global.scss';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const App = ({ Component, pageProps }) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
